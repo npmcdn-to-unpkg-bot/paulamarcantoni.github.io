f9247b9e288efa2091e080d7cfae98730b97c801
Agustin Parmisano <agustinparmisano@gmail.com>
2016-06-27 17:25:11 -0300

    Cambiando la manera de poner las imagenes

---
+++ images/pagina/convertidor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -ne 4 ]]; then
+  echo "Uso: directorio_origen directorio_destino LargoxAlto (ej: 300x190) NombreDestino."
+  exit 1
+fi
+
+origen=$1
+destino=$2
+tam=$3
+nombre=$4
+
+for (( i = 1; i < 6; i++ )); do
+  $(convert "${origen}/${4}${i}.jpg" -resize $3 "${destino}/${i}_${3}${4}.jpg")
+done
+
+exit 0
